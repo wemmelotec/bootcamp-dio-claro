22056d651a93dbea03046e4bfced16a7002c1e8f
Wellington <tonmelo@hotmail.com>
2024-08-03 09:04:19 -0300

    operacoesbasicas

---
+++ src/edu/collections/list/operacoesbasicas/Tarefa.java
@@ -0,0 +1,20 @@
+package edu.collections.list.operacoesbasicas;
+
+public class Tarefa {
+
+	private String descricao;
+
+	public Tarefa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@Override
+	public String toString() {
+		return descricao;
+	}
+
+}
