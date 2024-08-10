1e146f75c235df2b7eda7e4ec0e1ed830b4cb209
Wellington <tonmelo@hotmail.com>
2024-08-09 21:59:33 -0300

    feat: add desafios

---
+++ src/edu/basico/VerificacaoServico.java
@@ -0,0 +1,37 @@
+package edu.basico;
+
+import java.util.Scanner;
+
+public class VerificacaoServico {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// Entrada do serviço a ser verificado
+		String servico = scanner.nextLine().trim();
+
+		// Entrada do nome do cliente e os serviços contratados
+		String entradaCliente = scanner.nextLine().trim();
+
+		// Separando o nome do cliente e os serviços contratados
+		String[] partes = entradaCliente.split(",");
+		String nomeCliente = partes[0];
+		boolean contratado = false;
+
+		// TODO: Verifique se o serviço está na lista de serviços contratados
+		for (String string : partes) {
+			if (string.equals(servico)) {
+				contratado = true;
+                break;
+			}
+		}
+		if (contratado) {
+			System.out.println("Sim");
+		}else {
+			System.err.println("Não");
+		}
+
+		scanner.close();
+	}
+
+}
